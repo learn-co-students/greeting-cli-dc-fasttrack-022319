2aaeb308b97574ec0cafee8a7799dcccc88eed7f
Chuba Oraka <chuba.oraka@student.potomac.edu>
2019-02-24 15:51:03 +0000

    Done.

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,5 +1,10 @@
 # code the #greeting method here!
 
 def greeting(name)
+# <<<<<<< HEAD
   p "Hello #{name.strip}. It's nice to meet you."
+# =======
+  # p ""
+  # p "Hello #{name}. It's nice to meet you."
+# >>>>>>> e018c1974f0cd621f0e2b392ed9d93af72f69eba
 end
